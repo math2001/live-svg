700daf736041d7e2b3dd71a983ba1f1444273c47
Mathieu PATUREL <australie.p@gmail.com>
2017-09-17 15:22:01 +1000

    :bug: add only 1 file watcher
    even when more than one tab (in the browser) is open

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const path = require('path')
 const {access, constants: fs_constants, readFile} = require('fs')
 const url = require('url')
 const chokidar = require('chokidar')
+const opn = require('opn')
 
-const PORT = 5678
 const CWD = process.cwd()
 const sockets = {}
 const watchers = []
@@ -41,33 +41,53 @@ function getBrowserRefresher(filepath) {
 }
 
 
-app.get('/\*.svg', (req, res) => {
-    const filepath = path.join(CWD, req.path)
-    access(filepath, fs_constants.F_OK, (err) => {
-        if (err) {
-            res.status(404).send(`<h1>404</h1><p>Couldn't find <code>${filepath}</code> on this computer...</p>`)
-        } else {
+function main(IP, PORT, files=[]) {
+
+    app.get('/\*.svg', (req, res) => {
+        const filepath = path.join(CWD, req.path)
+        access(filepath, fs_constants.F_OK, (err) => {
+            if (err) {
+                return res.status(404)
+                    .send(`<h1>404</h1><p>Couldn't find <code>${filepath}</code> on this computer...</p>`)
+            }
             pageFrom(filepath, html => res.send(html))
+        })
+    })
+
+    io.on('connection', socket => {
+        console.info("Got a connection", socket.handshake.headers.referer)
+        const obj = url.parse(socket.handshake.headers.referer)
+        if (typeof sockets[obj.path] === 'undefined') {
+            sockets[obj.path] = []
+        }
+
+        sockets[obj.path].push(socket)
+        if (!watchers[obj.path]) {
+            watchers[obj.path] = chokidar.watch(path.join(CWD, obj.path))
+                .on('all', getBrowserRefresher(obj.path))
+        }
+        socket.on('disconnect', () => {
+            console.info(`Close socket for ${obj.path}`)
+            sockets[obj.path] = sockets[obj.path].filter(sock => socket.id !== sock.id)
+            if (sockets[obj.path].length === 0) {
+                console.info('And close watcher (no socket left)')
+                watchers[obj.path].close()
+            }
+        })
+    })
+
+    http.listen(PORT, () => {
+        console.info(`Listening on http://${IP}:${PORT}/`)
+        let _url;
+        for (let file of files) {
+            _url = `http://${IP}:${PORT}/${file}`
+            console.info('Opening', _url)
+            opn(_url)
         }
     })
-})
 
-io.on('connection', socket => {
-    console.info("Got a connection", socket.handshake.headers.referer)
-    const obj = url.parse(socket.handshake.headers.referer)
-    if (typeof sockets[obj.path] === 'undefined') {
-        sockets[obj.path] = []
-    }
+}
 
-    sockets[obj.path].push(socket)
-    watchers[obj.path] = chokidar.watch(path.join(CWD, obj.path)).on('all', getBrowserRefresher(obj.path))
-    socket.on('disconnect', () => {
-        console.info(`Stopped watching and close socket for ${obj.path}`)
-        watchers[obj.path].close()
-        sockets[obj.path] = sockets[obj.path].filter(sock => socket.id !== sock.id)
-    })
-})
+module.exports = main
 
-http.listen(PORT, () => {
-    console.info(`Listening on http://localhost:${PORT}/`)
-})
+main('localhost', 5678, ['sample.svg'])
